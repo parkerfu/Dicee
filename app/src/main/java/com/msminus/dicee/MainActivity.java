5bcf01b135b09e3438605c3bafe9fec1933c5586
parkerfu <b0044102@gmail.com>
2018-07-10 15:42:49 +0800

    Initial commit

---
+++ app/src/main/java/com/msminus/dicee/MainActivity.java
@@ -0,0 +1,50 @@
+package com.msminus.dicee;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //link the views in the layout xml file
+        final Button rollButton = (Button)findViewById(R.id.rollButton);
+        final ImageView leftDice = (ImageView)findViewById(R.id.image_leftDice);
+        final ImageView rightDice = (ImageView)findViewById(R.id.image_rightDice);
+
+        //define images in an array
+        final int[] imageDice={R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6};
+
+        //define buttonClickListener
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*Log.d("Dicee", "rollButton has been pressed!");*/
+                //create randonNumber generator
+                Random randomNumberGenerator = new Random();
+                //Make the random number generator spit out a new random number(Left)
+                int number = randomNumberGenerator.nextInt(6);
+                Log.d("LeftNumber",""+number);
+                leftDice.setImageResource(imageDice[number]);
+                //Make the random number generator spit out a new random number(Right)
+                number = randomNumberGenerator.nextInt(6);
+                Log.d("RightNumber",""+number);
+                rightDice.setImageResource(imageDice[number]);
+
+            }
+        });
+    }
+}
